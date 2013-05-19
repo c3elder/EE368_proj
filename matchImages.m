522bc0f1f62fb33d22275abf929cb2192682063e
c3elder <c3elder@gmail.com>
2013-05-19 12:50:14 -0700

    Convex Hull Implemented, Testing in Progress... need 64-bit machine

--- matchImages.m
+++ matchImages.m
@@ -12,14 +12,14 @@ img_ds = imresize(rawImage,downsize);
 
 % figure(1);imshow(img)
 % clear img
-figure(2);
-subplot(2,2,1);imshow(img_ds)
+% figure(2);
+% subplot(2,2,1);imshow(img_ds)
 
 % Convert to HSV
 img_ds_hsv = rgb2hsv(img_ds);
-subplot(2,2,2);imshow(img_ds_hsv(:,:,1))
-subplot(2,2,3);imshow(img_ds_hsv(:,:,2))
-subplot(2,2,4);imshow(img_ds_hsv(:,:,3))
+% subplot(2,2,2);imshow(img_ds_hsv(:,:,1))
+% subplot(2,2,3);imshow(img_ds_hsv(:,:,2))
+% subplot(2,2,4);imshow(img_ds_hsv(:,:,3))
 
 % Threshold
 thresh_global = graythresh(img_ds);
@@ -33,11 +33,11 @@ img_h_bw = im2bw(img_ds_hsv(:,:,1),thresh_h);
 img_s_bw = im2bw(img_ds_hsv(:,:,2),thresh_s);
 img_v_bw = im2bw(img_ds_hsv(:,:,3),thresh_v);
 
-figure(3);
-subplot(2,2,1);imshow(img_ds_bw)
-subplot(2,2,2);imshow(img_h_bw)
-subplot(2,2,3);imshow(img_s_bw)
-subplot(2,2,4);imshow(img_v_bw)
+% figure(3);
+% subplot(2,2,1);imshow(img_ds_bw)
+% subplot(2,2,2);imshow(img_h_bw)
+% subplot(2,2,3);imshow(img_s_bw)
+% subplot(2,2,4);imshow(img_v_bw)
 
 % Apply LoG Operator
 img_ds_log = edge(img_ds_bw,'log');
@@ -45,11 +45,11 @@ img_h_log = edge(img_h_bw,'log');
 img_s_log = edge(img_s_bw,'log');
 img_v_log = edge(img_v_bw,'log');
 
-figure(4);
-subplot(2,2,1);imshow(img_ds_log)
-subplot(2,2,2);imshow(img_h_log)
-subplot(2,2,3);imshow(img_s_log)
-subplot(2,2,4);imshow(img_v_log)
+% figure(4);
+% subplot(2,2,1);imshow(img_ds_log)
+% subplot(2,2,2);imshow(img_h_log)
+% subplot(2,2,3);imshow(img_s_log)
+% subplot(2,2,4);imshow(img_v_log)
 
 % Dilate
 box = ones(7,7);
@@ -58,11 +58,11 @@ img_h_dil = imdilate(img_h_log,box);
 img_s_dil = imdilate(img_s_log,box);
 img_v_dil = imdilate(img_v_log,box);
 
-figure(5);
-subplot(2,2,1);imshow(img_ds_dil)
-subplot(2,2,2);imshow(img_h_dil)
-subplot(2,2,3);imshow(img_s_dil)
-subplot(2,2,4);imshow(img_v_dil)
+% figure(5);
+% subplot(2,2,1);imshow(img_ds_dil)
+% subplot(2,2,2);imshow(img_h_dil)
+% subplot(2,2,3);imshow(img_s_dil)
+% subplot(2,2,4);imshow(img_v_dil)
 
 % Fill Holes
 img_ds_fill = imfill(img_ds_dil,'holes');
@@ -70,11 +70,11 @@ img_h_fill = imfill(img_h_dil,'holes');
 img_s_fill = imfill(img_s_dil,'holes');
 img_v_fill = imfill(img_v_dil,'holes');
 
-figure(6)
-subplot(2,2,1);imshow(img_ds_fill)
-subplot(2,2,2);imshow(img_h_fill)
-subplot(2,2,3);imshow(img_s_fill)
-subplot(2,2,4);imshow(img_v_fill)
+% figure(6)
+% subplot(2,2,1);imshow(img_ds_fill)
+% subplot(2,2,2);imshow(img_h_fill)
+% subplot(2,2,3);imshow(img_s_fill)
+% subplot(2,2,4);imshow(img_v_fill)
 
 % Label Regions
 img_v_label = bwlabel(img_v_fill,8);
@@ -126,10 +126,10 @@ for nRegion = 1:length(imgProps)
     end
 end % nRegion
 
-figure(7); imshow(img_v_final)
-figure(8); imshow(rgb2gray(img_ds).*img_v_final)
-figure(9); imshow(img_h_final)
-figure(10);imshow(rgb2gray(img_ds).*img_h_final)
+% figure(7); imshow(img_v_final)
+% figure(8); imshow(rgb2gray(img_ds).*img_v_final)
+% figure(9); imshow(img_h_final)
+% figure(10);imshow(rgb2gray(img_ds).*img_h_final)
 
 % Combine Filters
 img_hv_all = ((img_h_final + img_v_final) > 0);
@@ -137,59 +137,76 @@ figure(11);imshow(img_hv_all)
 
 % Separate the Bills
 img_hv_label = bwlabel(img_hv_all,8);
-imgProps = regionprops(img_hv_label, 'Orientation');
+imgProps = regionprops(img_hv_label, 'Orientation','ConvexImage','BoundingBox');
 
 % Write Each Bill Mask to a separate image
-img_hv_final = repmat(img_hv_all,[1,1,length(imgProps)]);
+img_hv_final = zeros([size(img_hv_all),length(imgProps)]);%repmat(img_hv_all,[1,1,length(imgProps)]);
 for nRegion = 1:length(imgProps)
     idx = find(img_hv_label == nRegion);
+    convHull = imgProps(nRegion).ConvexImage;
+    box = imgProps(nRegion).BoundingBox;
+%     disp([box(3), box(4), size(convHull)])
+    
+    img_hv_final(box(2):box(2)+box(4)-1,box(1):box(1)+box(3)-1,nRegion) = convHull;
+    figure(11+nRegion);imshow(img_hv_final(:,:,nRegion))
+%    img_hv_final(:,:,nRegion) = imgProps(nRegion).ConvexImage;
     % Black out blob nRegion in all but the nRegion-th image
-    for i = 1:length(imgProps)
-        if (i ~= nRegion)
-            img_tmp = img_hv_final(:,:,i);
-            img_tmp(idx) = 0;
-            img_hv_final(:,:,i) = img_tmp;
-        end
-    end
+%     for i = 1:length(imgProps)
+%         if (i ~= nRegion)
+%             img_tmp = img_hv_final(:,:,i);
+%             img_tmp(idx) = 0;
+%             img_hv_final(:,:,i) = img_tmp;
+%         end
+%     end
 end
+
 % ****************END OF SEGMENTATION**************************************
 fprintf('End Segmentation, Start SIFTing...\n')
 
 % **********************START SIFTING**************************************
 % sample = rgb2gray(rawImage);
-sample  = rgb2gray(rawImage.* ...
-          repmat(imresize(img_hv_final(:,:,1),1/downsize,'nearest'),[1,1,3]));
-figure(1);imshow(sample)
-clearvars -except sample
 
+clearvars -except rawImage img_hv_final downsize
 load('Bill Images/goldenSiftResults.mat');
-outputStruct = struct();
+num_bills = size(img_hv_final,3);
+outputStruct = struct();  
+outputStruct.Country = cell(1,num_bills);
+outputStruct.ImgLoc = cell(1,num_bills);
+outputStruct.Features = cell(1,num_bills);
 
-
-[Fsamp,Dsamp] = vl_sift(single(sample), 'PeakThresh', 0.05);
-Dsamp = single(Dsamp);
-disp(Dsamp)
-
-[goldenRows, goldenCols] = size(goldenSiftResults);
-matchSum = zeros(goldenRows,1);
-
-for j = 1:goldenRows;
-    [DsampRows, DsampCols] = size(Dsamp);
-    featureMatch = zeros(DsampCols, 2);
-    for i=1:DsampCols
-        % use Flann or kdtree for searching
-        % http://www.vlfeat.org/overview/kdtree.html
+% SIFT each bill individually
+for bill = 1:size(img_hv_final,3)
+    disp(sprintf('SIFTING Bill# %d',bill))
+    sample  = rgb2gray(rawImage.* ...
+              repmat(imresize(img_hv_final(:,:,bill),1/downsize,'nearest'),[1,1,3]));
+    figure(bill+11);imshow(sample);title(sprintf('Bill#: %d',bill))
+    
+    [Fsamp,Dsamp] = vl_sift(single(sample), 'PeakThresh', 0.05);
+    Dsamp = single(Dsamp);
+%     disp(Dsamp)
+    
+    [goldenRows, goldenCols] = size(goldenSiftResults);
+    matchSum = zeros(goldenRows,1);
+    
+    for j = 1:goldenRows;
+        [DsampRows, DsampCols] = size(Dsamp);
+        featureMatch = zeros(DsampCols, 2);
+        for i=1:DsampCols
+            % use Flann or kdtree for searching
+            % http://www.vlfeat.org/overview/kdtree.html
+            
+            [index, distance] = vl_kdtreequery(goldenSiftResults{j,7}, goldenSiftResults{j,6}, Dsamp(:,i));
+            featureMatch(i, :) = [index, distance];
+        end
+        [featureMatchSorted ind] = sort(featureMatch(:, 2));
         
-        [index, distance] = vl_kdtreequery(goldenSiftResults{j,7}, goldenSiftResults{j,6}, Dsamp(:,i));
-        featureMatch(i, :) = [index, distance];
+        matchSum(j) = sum(featureMatchSorted(1:20));
     end
-    [featureMatchSorted ind] = sort(featureMatch(:, 2));
-
-    matchSum(j) = sum(featureMatchSorted(1:20));
+    
+    [val ind]= min(matchSum);
+    
+    outputStruct.Country{bill} = goldenSiftResults{ind, 1};
+    outputStruct.ImgLoc{bill} = goldenSiftResults{ind, 2};
+    outputStruct.Features{bill} = Dsamp;
+    
 end
-
-[val ind]= min(matchSum);
-
-outputStruct.Country = { goldenSiftResults{ind, 1} };
-outputStruct.ImgLoc = { goldenSiftResults{ind, 2} };
-outputStruct.Features = Dsamp;
