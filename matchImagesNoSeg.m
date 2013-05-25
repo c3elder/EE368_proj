25205145c344eb344e0c0b443905b042f1dac0b1
Michael Digman <digman@digmansworld.com>
2013-05-24 17:34:04 -0700

    More bill images, cleaned up tester code
    Note: all bill images pass even without GCC turned on. Using GCC for
    coordinate transforms doesn't always generated nice values. Will
    implement the linear transform next.

--- matchImagesNoSeg.m
+++ matchImagesNoSeg.m
@@ -6,7 +6,7 @@ terminalOutput = false;
 numRepeats = 3;
 useGCC = true;
 maxFeatures = 500;
-
+showUI = false;
 
 % ALGORITHM START
 load('Bill Images/goldenSiftResults.mat');
@@ -42,11 +42,11 @@ Dsamp = single(Dsamp);
 
 [goldenRows, goldenCols] = size(goldenSiftResults);
 matchSum = zeros(goldenRows,1);
-
+Hs = cell(goldenRows,1);
 for j = 1:goldenRows;
     
     clear H score ok ;
-        
+    
     Fgolden = goldenSiftResults{j, 5};
     Dgolden = goldenSiftResults{j, 6};
     
@@ -71,41 +71,45 @@ for j = 1:goldenRows;
         matches = matches(:, clearSelector);
         scores = scores(:, clearSelector);
         
-        matchSum(j) = sum(clearSelector);
+        %there is again another chance for matches to be empty, make sure
+        %it's not
         
-        %GCC check
-        if useGCC
-            numMatches = size(matches,2) ;
-            X1 = Fsamp(1:2,matches(1,:)) ; X1(3,:) = 1 ;
-            X2 = Fgolden(1:2,matches(2,:)) ; X2(3,:) = 1 ;
-            
-            score = zeros(100,1);
-            ok = cell(100,1);
-            H = cell(100,1);
-            for t = 1:100
-                % estimate homograpyh
-                subset = vl_colsubset(1:numMatches, 4) ;
-                A = [] ;
-                for i = subset
-                    A = cat(1, A, kron(X1(:,i)', vl_hat(X2(:,i)))) ;
+        if ~isempty(matches)
+            matchSum(j) = sum(clearSelector);
+            %GCC check
+            if useGCC
+                numMatches = size(matches,2) ;
+                X1 = Fsamp(1:2,matches(1,:)) ; X1(3,:) = 1 ;
+                X2 = Fgolden(1:2,matches(2,:)) ; X2(3,:) = 1 ;
+                
+                score = zeros(100,1);
+                ok = cell(100,1);
+                H = cell(100,1);
+                for t = 1:100
+                    % estimate homograpyh
+                    subset = vl_colsubset(1:numMatches, 4) ;
+                    A = [] ;
+                    for i = subset
+                        A = cat(1, A, kron(X1(:,i)', vl_hat(X2(:,i)))) ;
+                    end
+                    [U,S,V] = svd(A);
+                    H{t} = reshape(V(:,9),3,3) ;
+                    
+                    % score homography
+                    X2_ = H{t} * X1 ;
+                    du = X2_(1,:)./X2_(3,:) - X2(1,:)./X2(3,:) ;
+                    dv = X2_(2,:)./X2_(3,:) - X2(2,:)./X2(3,:) ;
+                    ok{t} = (du.*du + dv.*dv) < 6*6 ;
+                    score(t) = sum(ok{t}) ;
                 end
-                [U,S,V] = svd(A);
-                H{t} = reshape(V(:,9),3,3) ;
-
-                % score homography
-                X2_ = H{t} * X1 ;
-                du = X2_(1,:)./X2_(3,:) - X2(1,:)./X2(3,:) ;
-                dv = X2_(2,:)./X2_(3,:) - X2(2,:)./X2(3,:) ;
-                ok{t} = (du.*du + dv.*dv) < 6*6 ;
-                score(t) = sum(ok{t}) ;
+                
+                [score, best] = max(score) ;
+                H = H{best} ;
+                Hs{j} = H;
+                ok = ok{best} ;
+                matchSum(j) = sum(ok);
             end
-
-            [score, best] = max(score) ;
-            H = H{best} ;
-            ok = ok{best} ;
-            matchSum(j) = sum(ok);
         end
-        
     end
     if terminalOutput
         fprintf('%d: matches %d, GCC matches %d, mean score %d\n', j, numel(matches)/2, matchSum(j), mean(scores))
@@ -115,13 +119,13 @@ for j = 1:goldenRows;
         if ~isempty(matches)
             f1 = Fsamp;
             f2 = Fgolden;
-
+            
             goldenDir = ['Bill Images/' goldenSiftResults{j, 1} '/' goldenSiftResults{j, 2}];
             im2 = imread(goldenDir);
-
+            
             dh1 = max(size(im2,1)-size(im1,1),0) ;
             dh2 = max(size(im1,1)-size(im2,1),0) ;
-
+            
             figure; clf ;
             subplot(2,1,1) ;
             imagesc([padarray(im1,dh1,'post') padarray(im2,dh2,'post')]) ;
@@ -130,7 +134,7 @@ for j = 1:goldenRows;
                 [f1(2,matches(1,:));f2(2,matches(2,:))]) ;
             title(sprintf('%d tentative matches', numMatches)) ;
             axis image off ;
-
+            
             subplot(2,1,2) ;
             imagesc([padarray(im1,dh1,'post') padarray(im2,dh2,'post')]) ;
             o = size(im1,2) ;
@@ -141,16 +145,37 @@ for j = 1:goldenRows;
                 100*sum(ok)/numMatches, ...
                 numMatches)) ;
             axis image off ;
-
+            
             drawnow ;
         else
-           fprintf('DEBUG: Figure %d not shown because there are 0 matches\n', j); 
+            fprintf('DEBUG: Figure %d not shown because there are 0 matches\n', j);
         end
     end
     
 end
 
 [val, ind] = max(matchSum);
+
+if showUI
+    %map edges
+    H = Hs{ind};
+    xGolden = 10;
+    yGolden = 10;
+    
+    pa = [xGolden; yGolden; 1];
+    pb = H*pa;
+    pb = pb/pb(3);
+    xSample = pb(1)
+    ySample = pb(2)
+    
+%     z =  H(3,1)*xGolden + H(3,2)*yGolden + H(3,3);
+%     xSample = ( H(1,1)*xGolden + H(1,2)*yGolden + H(1,3) )/z
+%     ySample = ( H(2,1)*xGolden + H(2,2)*yGolden + H(2,3) )/z
+    imshow(rawImage)
+    hold on
+    plot(round(xSample),round(ySample),'o');
+end
+
 outputStruct.Country = { goldenSiftResults{ind, 1} };
 outputStruct.ImgLoc = { goldenSiftResults{ind, 2} };
 outputStruct.Features = { Dsamp };
