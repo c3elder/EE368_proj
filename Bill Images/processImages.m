86289df0e209a55790f552944a77c0251a37b6cb
Michael Digman <digman@digmansworld.com>
2013-05-28 00:30:37 -0700

    Final tweaks on the optimized matchImages
    Added parallel match calculation as well as parallel GCC calculation for
    about a 2 - 3 sec speed up. Seems to match most images with a max of 4
    bills. Sometimes works well for 5 - 7.

--- Bill Images/processImages.m
+++ Bill Images/processImages.m
@@ -25,12 +25,18 @@ for i=1:rows-1
     
     imInUse = imread([country, folderSlash, location]);
     [imR, imC, N] = size(imInUse);
+    %imInUse = rgb2ycbcr(imInUse);
+    %[f, d] = vl_sift(single(imInUse(:,:,1)), 'PeakThresh', thresh);
     [f, d] = vl_sift(single(rgb2gray(imInUse)), 'PeakThresh', thresh);
     
     %eliminate features with very small scale
     [sortedList sortedInd ] = sort(f(1,:), 'descend');
-    f = f(:, sortedInd(1:maxFeatures));
-    d = d(:, sortedInd(1:maxFeatures));
+    smallestFeatNum = min(size(f,2), maxFeatures);
+    if smallestFeatNum < maxFeatures
+       fprintf('not enough features\n'); 
+    end
+    f = f(:, sortedInd(1:smallestFeatNum));
+    d = d(:, sortedInd(1:smallestFeatNum));
     
     kdTree = vl_kdtreebuild(single(d));
     goldenSiftResults(i,:) = {country, location, value, thresh, f, single(d), unitValue, imR, imC};
--- matchImagesMultiOptimized.m
+++ matchImagesMultiOptimized.m
@@ -1,25 +1,43 @@
-function outputStruct = matchImagesMultiOptimized(inputImagePath)
+function outputStruct = matchImagesMultiOptimized(inputImagePath, output_img_path)
+
+%run('vlfeat-0.9.16\toolbox\vl_setup');
+
+if nargin < 2
+    output_img_path = 'C:\xampp-portable\htdocs\output\test.jpg';
+end
 
 tic
 
 % SETUP VARS
-debug = false;
-numRepeats = 3;
-maxFeatures = 500;
-numRansacIterations = 100;
-showUI = false;
+
+numRepeats = 1;
+maxFeatures = 3000;
+numRansacIterations = 400;
+ubcThresh = 2.0;
 postGCCMatchThreshold = 10;
 GCCNumRandFeatures = 4;
-peakThreshold = 5;
-textSize = 18;
-textBorder = 7;
-earlyTermThresh = 0.5;
+peakThreshold = 0.1;
+earlyTermThresh = 0.2;
+gccScoreThreshold = 30;
+centroidGap = 80;
+
+debug = false;
+showUI = true;
+droidCamera = true;
+textSize = 30;
+textBorder = 12;
 
 % ALGORITHM START
 load('Bill Images/goldenSiftResults.mat');
 
-rawImage = imread(inputImagePath);
-rawImage = imresize(rawImage, [800 NaN]);
+rawImage = imread(inputImagePath, 'jpg');
+if droidCamera
+   rawImage = rawImage(1:2:end,1:2:end,:);
+else
+    rawImage = rawImage(1:3:end,1:3:end,:);
+    %rawImage = imresize(rawImage, [800 NaN]);
+end
+
 if debug %store image for showing later
     im1 = rawImage;
 end
@@ -27,6 +45,8 @@ end
 [M N C] = size(rawImage);
 if C==3
     sample = rgb2gray(rawImage);
+    %sample = rgb2ycbcr(rawImage);
+    %sample = sample(:,:,1);
 else
     sample = rawImage;
 end
@@ -39,6 +59,8 @@ outputStruct.matchSum = {};
 outputStruct.matchSumOrig = {};
 outputStruct.tformMatrix = {};
 outputStruct.goldenIndex = [];
+outputStruct.centroid = [];
+outputStruct.border = {};
 
 % SIFT INPUT
 [Fsamp,Dsamp] = vl_sift(single(sample), 'PeakThresh', peakThreshold);
@@ -57,18 +79,42 @@ Dsamp = single(Dsamp);
 [goldenRows, goldenCols] = size(goldenSiftResults);
 
 billsToBeFound = true;
-initialRun = true;
 
 matchesArrayOrig = cell(goldenRows,1);
 matchesArray = cell(goldenRows,1);
 
+%Get the matches in parallel
+parfor j=1:goldenRows;
+    Dgolden = goldenSiftResults{j, 6};
+    [matches, scores]=vl_ubcmatch(Dsamp, Dgolden, ubcThresh);
+    
+    if ~isempty(matches)
+        %for some unknown reason, tons of features end up mapping to the same
+        %point in the golden bill, remove repeats
+        uniqueValues = unique(matches(2,:));
+        uniqueValueSums = zeros(length(uniqueValues), 1);
+        for k = 1:length(uniqueValues)
+            uniqueValueSums(k) = sum(matches(2,:)==uniqueValues(k));
+        end
+        repeatedValues = uniqueValues(uniqueValueSums>numRepeats); %if more than numRepeats of same descriptor
+        clearSelector = true(1, length(matches(2,:)));
+        for k=1:length(repeatedValues)
+            clearSelector = clearSelector & (matches(2, :) ~= repeatedValues(k));
+        end
+        matches = matches(:, clearSelector);
+        %scores = scores(:, clearSelector);
+    end
+    
+    matchesArrayOrig{j} = matches;
+end
+
+%Iterative to find the bills
 while billsToBeFound
     % SETUP
     matchSum = zeros(goldenRows,1);
     matchSumOrig = zeros(goldenRows,1);
     Hs = cell(goldenRows,1);
-    
-    
+
     % GET INITIAL MATCHES
     for j = 1:goldenRows;
         
@@ -77,32 +123,7 @@ while billsToBeFound
         
         matchSum(j) = 0;
         matchSumOrig(j) = 0;
-        if initialRun %if it's the first run, record all the matches
-            [matches, scores]=vl_ubcmatch(Dsamp, Dgolden, 1.5);
-
-            %sanity check: are there any matches? sometimes not.         
-            if ~isempty(matches)
-
-                %for some unknown reason, tons of features end up mapping to the same
-                %point in the golden bill, remove repeats
-                uniqueValues = unique(matches(2,:));
-                uniqueValueSums = zeros(length(uniqueValues), 1);
-                for k = 1:length(uniqueValues)
-                    uniqueValueSums(k) = sum(matches(2,:)==uniqueValues(k));
-                end
-                repeatedValues = uniqueValues(uniqueValueSums>numRepeats); %if more than numRepeats of same descriptor
-                clearSelector = true(1, length(matches(2,:)));
-                for k=1:length(repeatedValues)
-                    clearSelector = clearSelector & (matches(2, :) ~= repeatedValues(k));
-                end
-                matches = matches(:, clearSelector);
-                scores = scores(:, clearSelector);
-            end
-            
-            matchesArrayOrig{j} = matches;
-        else %extract the old / modified matches
-            matches = matchesArrayOrig{j};
-        end
+        matches = matchesArrayOrig{j}; %extract from earlier run
         
         %there is again another chance for matches to be empty, make sure it's not
         if ~isempty(matches)
@@ -116,13 +137,27 @@ while billsToBeFound
             numMatches = size(matches,2) ;
             
             %GCC check
-            X1 = Fsamp(1:2,matches(1,:)) ; X1(3,:) = 1 ;
-            X2 = Fgolden(1:2,matches(2,:)) ; X2(3,:) = 1 ;
+            X1 = Fgolden(1:2,matches(2,:)) ; X1(3,:) = 1 ;
+            X2 = Fsamp(1:2,matches(1,:)) ; X2(3,:) = 1 ;
+            
             
             score = zeros(numRansacIterations,1);
             ok = cell(numRansacIterations,1);
             H = cell(numRansacIterations,1);
-            for t = 1:numRansacIterations
+            
+            %pull geometry for GCC sanity check
+            border = 5;
+            imR = goldenSiftResults{j, 8};
+            imC = goldenSiftResults{j, 9};
+            pa = zeros(2,4);
+            pa(:,1) = [-border; -border];
+            pa(:,2) = [-border; imR+border];
+            pa(:,3) = [imC+border; imR+border];
+            pa(:,4) = [imC+border; -border];
+            pa = [pa; ones(1, numel(pa)/2)];
+            centroids = outputStruct.centroid;
+            %run ransac in parallel
+            parfor t = 1:numRansacIterations
                 %subset = vl_colsubset(1:numMatches, 4) ;
                 subset = randperm(numMatches,GCCNumRandFeatures);
                 A = [];
@@ -138,8 +173,30 @@ while billsToBeFound
                 X2_ = H{t} * X1 ;
                 du = X2_(1,:)./X2_(3,:) - X2(1,:)./X2(3,:) ;
                 dv = X2_(2,:)./X2_(3,:) - X2(2,:)./X2(3,:) ;
-                ok{t} = (du.*du + dv.*dv) < 6*6 ;
+                ok{t} = (du.*du + dv.*dv) < gccScoreThreshold ;
                 score(t) = sum(ok{t}) ;
+                
+                % sanity check: if H produces a centroid in the middle of a
+                % previously found bill, kill it
+                if ~isempty(centroids)
+                    xSample = zeros(1,numel(pa)/3);
+                    ySample = zeros(1,numel(pa)/3);
+                    for i=1:numel(pa)/3
+                        paInUse = pa(:,i);
+                        pb = H{t}*paInUse;
+                        pb = pb/pb(3);
+                        xSample(i) = pb(1);
+                        ySample(i) = pb(2);
+                    end
+                    Cx = round(mean(xSample));
+                    Cy = round(mean(ySample));
+                    if any( (abs(centroids(1,:)-Cx)<centroidGap) & (abs(centroids(2,:)-Cy)<centroidGap) )
+                      % fprintf('centroid in close proximity to another\n');
+                      score(t) = 0;
+                    end
+                end
+                
+                
             end
             
             [valScores, best] = max(score) ;
@@ -188,13 +245,40 @@ while billsToBeFound
         end
         
     end
-    
-    initialRun = false; %completed the first run
-    
+        
     [val, ind] = max(matchSum);
-    %val
+    %final sanity check
+    
     if val>postGCCMatchThreshold        
+        %calculate the centroid and border using H
+        border = 5;
+        imR = goldenSiftResults{ind, 8};
+        imC = goldenSiftResults{ind, 9};
+        pa = zeros(2,4);
+        pa(:,1) = [-border; -border];
+        pa(:,2) = [-border; imR+border];
+        pa(:,3) = [imC+border; imR+border];
+        pa(:,4) = [imC+border; -border];
+        H = Hs{ind};
+        
+        pa = [pa; ones(1, numel(pa)/2)];
+        Cx = 0;
+        Cy = 0;
+        xSample = zeros(1,numel(pa)/3);
+        ySample = zeros(1,numel(pa)/3);
+        for i=1:numel(pa)/3
+            paInUse = pa(:,i);
+            pb = H*paInUse;
+            pb = pb/pb(3);
+            xSample(i) = pb(1);
+            ySample(i) = pb(2);
+        end
+        Cx = round(mean(xSample));
+        Cy = round(mean(ySample));
+        
         %keep the relevant results
+        outputStruct.centroid(1:2, end+1) = [Cx; Cy];
+        outputStruct.border{end+1} = [xSample; ySample];
         outputStruct.goldenIndex(end+1) = ind;
         outputStruct.tformMatrix = [outputStruct.tformMatrix, { Hs{ind} }];
         outputStruct.Country = [outputStruct.Country, { goldenSiftResults{ind, 1} }];
@@ -205,9 +289,7 @@ while billsToBeFound
              
         %remove the matched features from Dsamp and Fsamp entirely
         matches = matchesArray{ind};
-        %Fsamp(:, matches(1,:)) = [];
-        %Dsamp(:, matches(1,:)) = [];
-        %remove any rows in common
+        
         matchSumOrigNext = zeros(goldenRows,1);
         for j=1:goldenRows
             oldMatches = matchesArrayOrig{j};
@@ -242,56 +324,53 @@ if showUI
     imshow(rawImage)
     hold on
     
+    grandTotal = 0;
     for j = 1:length(outputStruct.goldenIndex)
         ind = outputStruct.goldenIndex(j);
-        H = outputStruct.tformMatrix{j};
-        imR = goldenSiftResults{ind, 8};
-        imC = goldenSiftResults{ind, 9};
         
-        invH = inv(H);
-        border = 5;
-
-        pa = zeros(2,4);
-        pa(:,1) = [-border; -border];
-        pa(:,2) = [-border; imR+border];
-        pa(:,3) = [imC+border; imR+border];
-        pa(:,4) = [imC+border; -border];
-
-        
-        pa = [pa; ones(1, numel(pa)/2)];
-        Cx = 0;
-        Cy = 0;
-        for i=1:numel(pa)/3
-            paInUse = pa(:,i);
-            pb = invH*paInUse;
-            pb = pb/pb(3);
-            xSample = pb(1);
-            Cx = Cx + xSample;
-            ySample = pb(2);
-            Cy = Cy + ySample;
-            plot(round(xSample),round(ySample),'*k');
-        end
-        Cx = round(Cx/4);
-        Cy = round(Cy/4);
+        Cx = outputStruct.centroid(1,j);
+        Cy = outputStruct.centroid(2,j);
+        border = outputStruct.border{j};
+        plot(round(border(1,:)),round(border(2,:)),'*k');
         plot(Cx, Cy, 'xr')
         
         nativeBillVal = goldenSiftResults{ind, 3};
         USDBillVal = nativeBillVal*goldenSiftResults{ind, 7};
-        createText(Cx,Cy,[goldenSiftResults{ind, 1} ' ' num2str(nativeBillVal)], textSize);
-        createText(Cx,Cy+textSize+textBorder,sprintf('$%.2f', USDBillVal), textSize);
         
+        [subSampImgR subSampImgC] = size(sample);
+        
+        textEdgeBarrier = 80;
+        if Cx < textEdgeBarrier
+            Cx = textEdgeBarrier;
+        elseif Cx > subSampImgC - textEdgeBarrier
+            Cx = subSampImgC - textEdgeBarrier;
+        end
+        if Cy < textEdgeBarrier
+            Cy = textEdgeBarrier;
+        elseif Cy > subSampImgR - textEdgeBarrier
+            Cy = subSampImgR - textEdgeBarrier;
+        end
+        
+        createText(Cx,Cy,[goldenSiftResults{ind, 1} ' ' num2str(nativeBillVal)], textSize-5, 'center');
+        createText(Cx,Cy+textSize+textBorder,sprintf('$%.2f', USDBillVal), textSize, 'center');
+        grandTotal = grandTotal + USDBillVal;
     end
+    createText(50,50,sprintf('Total $%.2f', grandTotal), textSize-5, 'left');
+    
+    f = getframe(gca);
+    im = frame2im(f);
+    imwrite(im,output_img_path, 'jpg');
 
 end
 
 end
 
-function createText(Cx,Cy, inpString, textSize)
-text(Cx+1,Cy+1,inpString, 'HorizontalAlignment','center', 'FontSize', textSize, 'Color', [0 0 0])
-text(Cx-1,Cy-1,inpString, 'HorizontalAlignment','center', 'FontSize', textSize, 'Color', [0 0 0])
-text(Cx+1,Cy-1,inpString, 'HorizontalAlignment','center', 'FontSize', textSize, 'Color', [0 0 0])
-text(Cx-1,Cy+1,inpString, 'HorizontalAlignment','center', 'FontSize', textSize, 'Color', [0 0 0])
-text(Cx,Cy,inpString, 'HorizontalAlignment','center', 'FontSize', textSize, 'Color', [1 1 1])
+function createText(Cx,Cy, inpString, textSize, alignTo)
+text(Cx+1,Cy+1,inpString, 'HorizontalAlignment',alignTo, 'FontSize', textSize, 'Color', [0 0 0])
+text(Cx-1,Cy-1,inpString, 'HorizontalAlignment',alignTo, 'FontSize', textSize, 'Color', [0 0 0])
+text(Cx+1,Cy-1,inpString, 'HorizontalAlignment',alignTo, 'FontSize', textSize, 'Color', [0 0 0])
+text(Cx-1,Cy+1,inpString, 'HorizontalAlignment',alignTo, 'FontSize', textSize, 'Color', [0 0 0])
+text(Cx,Cy,inpString, 'HorizontalAlignment', alignTo, 'FontSize', textSize, 'Color', 'y')
 end
 
 function [estX1, estX2] = homographEstimation(th, x1, x2)
